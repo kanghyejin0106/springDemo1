6a0dca588245da66e922e4413d75cfb0a65d4a11
Kang Hye Jin <34128604+kanghyejin0106@users.noreply.github.com>
2021-05-14 12:28:40 +0900

    spring practice project
    spring Demo practice project.
    Study from online lectures.

---
+++ src/main/java/com/mycompany/myspringdemo/GetSum.java
@@ -0,0 +1,23 @@
+package com.mycompany.myspringdemo;
+
+public class GetSum {
+	private int aa;
+	private int bb;
+	public void sum() {
+		System.out.println("더하기");
+		int result = aa+bb;
+		System.out.println("합: "+result);
+	}
+	public int getAa() {
+		return aa;
+	}
+	public void setAa(int aa) {
+		this.aa = aa;
+	}
+	public int getBb() {
+		return bb;
+	}
+	public void setBb(int bb) {
+		this.bb = bb;
+	}
+}
---
+++ src/main/java/com/mycompany/myspringdemo/Main.java
@@ -0,0 +1,13 @@
+package com.mycompany.myspringdemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GetSum getsum = new GetSum();
+		getsum.setAa(100);
+		getsum.setBb(200);
+		getsum.sum();
+	}
+
+}
